c75ae92b8263bd58807f348b724c1e97387dbb0b
beniamin <beniaminpawelec@hotmail.com>
2020-11-15 14:23:23 +0100

    Comment test

--- Scheduler.App/Program.cs
+++ Scheduler.App/Program.cs
@@ -46,26 +46,26 @@ namespace Scheduler.App
 
             //testy
 
-            var customerDataFilePath = Startup.Configuration.GetValue<string>(global.CustomerDataFile);
-            var mailDeliveryDirectory = FilePathFactory(Startup.Configuration.GetValue<string>(global.MailDeliveryDirectory));
-            var messageSendTimeInterval = Startup.Configuration.GetValue<string>(global.MessageSendTimeInterval);
-            var messageTemplate = Startup.Configuration.GetValue<string>(global.MessageTemplate);
-            var razorTemplate = Startup.Configuration.GetValue<string>(global.RazorTemplateFile);
+            //var customerDataFilePath = Startup.Configuration.GetValue<string>(global.CustomerDataFile);
+            //var mailDeliveryDirectory = FilePathFactory(Startup.Configuration.GetValue<string>(global.MailDeliveryDirectory));
+            //var messageSendTimeInterval = Startup.Configuration.GetValue<string>(global.MessageSendTimeInterval);
+            //var messageTemplate = Startup.Configuration.GetValue<string>(global.MessageTemplate);
+            //var razorTemplate = Startup.Configuration.GetValue<string>(global.RazorTemplateFile);
 
-            ILogger logger = new Logger(Startup.Configuration);
-            ICsvHelper csvHelper = new Csv();
-            IScheduler scheduler = new Scheduler.Impl.Scheduler.Scheduler(logger);
-            IMailer mailer = new Mailer(messageTemplate, razorTemplate, mailDeliveryDirectory);
+            //ILogger logger = new Logger(Startup.Configuration);
+            //ICsvHelper csvHelper = new Csv();
+            //IScheduler scheduler = new Scheduler.Impl.Scheduler.Scheduler(logger);
+            //IMailer mailer = new Mailer(messageTemplate, razorTemplate, mailDeliveryDirectory);
 
-            var mailerJobSettings = MailerJobSettingsFactory(Startup.Configuration, logger, csvHelper, mailer);
-            IJob job = new MailerJob(nameof(MailerJob), messageSendTimeInterval, new CancellationToken(), mailerJobSettings);
+            //var mailerJobSettings = MailerJobSettingsFactory(Startup.Configuration, logger, csvHelper, mailer);
+            //IJob job = new MailerJob(nameof(MailerJob), messageSendTimeInterval, new CancellationToken(), mailerJobSettings);
 
-            IScheduler scheduler = Startup.ServiceSettings.Scheduler;
-            IJob job = Startup.ServiceSettings.Job;
+            //IScheduler scheduler = Startup.ServiceSettings.Scheduler;
+            //IJob job = Startup.ServiceSettings.Job;
 
-            scheduler.AddJob(job);
+            //scheduler.AddJob(job);
 
-            await scheduler.StartAsync();
+            //await scheduler.StartAsync();
         }
 
 
